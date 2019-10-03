a2e2e26426cd58f270dcfeefe9b75f0106ab2de8
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2019-10-03 19:33:59 +0500

    The same problem continues...!

---
+++ Faulty Task 5.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        ' This is task 5 where a string would be inversed
+        Dim myStr As String
+        Dim myChar As Char
+        Dim loopno As Integer
+        Dim InvChar As Char
+        Dim InvStr As String
+
+        myChar = ""
+        loopno = 1
+        InvChar = ""
+        InvStr = ""
+
+        Console.Write("Enter the string which needs to be inversed")
+        myStr = Console.ReadLine
+        InvChar = Mid(myStr, Len(myStr), 1)
+        InvStr = InvStr & InvChar
+        For loopno = 1 To Len(myStr)
+            InvChar = Mid(myStr, Len(myStr) - loopno, 1)
+            InvStr = InvStr & InvChar
+        Next
+        Console.WriteLine("The inverse of the string is " & InvStr)
+        Console.ReadKey()
+    End Sub
+
+End Module
