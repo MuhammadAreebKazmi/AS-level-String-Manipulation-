fb8977f6f15fa428489e0b773a55f0b438bb8954
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2019-10-04 22:13:27 +0500

    The program checks whether the input is a pangram.

---
+++ Pangram COmpleted Check.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myStr As String = ""
+        Dim IsPangram As Boolean = False
+        Dim character As Char = ""
+        Dim ascii As Integer = 0
+        Dim Length As Integer = 0
+        Dim str As String = ""
+
+        Console.Write("Enter the string to check whether is a pangram or not: ")
+        myStr = Console.ReadLine
+
+        'It would be better if we had used a validation for length check of less than 26 characters
+
+        str = UCase(myStr)
+        For ascii = 65 To 90
+            For Length = 1 To Len(str)
+                character = Mid(str, Length, 1)
+                If Asc(character) = ascii Then
+                    IsPangram = True
+                    Exit For
+                Else : IsPangram = False
+                End If
+            Next
+        Next
+        If IsPangram = True Then
+            Console.WriteLine("It is a pangram ")
+            Console.ReadKey()
+        Else : Console.WriteLine("It is not a pangram ")
+            Console.ReadKey()
+        End If
+    End Sub
+
+End Module
