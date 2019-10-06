32ad4eacc73a61f2967bb1de0e740e948751b978
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2019-10-06 19:19:32 +0500

    Task 2 checked with Pseudocode

---
+++ Middle Word Completed Task 2.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str As String = ""
+        Dim X As Integer = 0
+        Dim Y As Integer = 0
+        Dim NewStr As String = ""
+        Dim Midword As String = ""
+
+        Console.Write("Enter the string of three words: ")
+        str = Console.ReadLine
+
+        X = InStr(str, " ")
+        Y = InStr(X + 1, str, " ")
+
+        Midword = Mid(str, X + 1, Y - X - 1)
+
+        Console.WriteLine("The middle word of the string is: " & Midword)
+        Console.ReadKey()
+    End Sub
+
+End Module
