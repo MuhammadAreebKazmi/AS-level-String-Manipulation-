7372edf01e0f9dbe0808a61089141b98055df3af
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2019-10-11 22:00:31 +0500

    Add files via upload

---
+++ Partially Complete task 3.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str As String = ""
+        Dim loop1 As Integer = 0
+        Dim Spaceposn As Integer = 0
+        Dim AfterSpace As Integer = 0
+        Dim Character As Char = ""
+        Dim word As String = ""
+        Const Space = " "
+        Dim Newstr As String = ""
+
+        Console.Write("Enter the string and enter a space at the end: ")
+        str = Console.ReadLine
+
+        For loop1 = 1 To Len(str)
+            Character = Mid(str, loop1, 1)
+            Newstr = Newstr & Character
+            If Character = Space Then
+                Console.WriteLine(Newstr)
+                Newstr = ""
+            End If
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
