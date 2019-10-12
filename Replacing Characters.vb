6eb5bfb8ea3d9f1b80c56f7144303237693b322e
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2019-10-12 21:39:21 +0500

    Program to replace characters
    The characters will be replaced like if it is a # in the string so will be replaced by *.

---
+++ Replacing Characters.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mystr As String = ""
+        Dim Newstr As String = ""
+        Dim chr1 As Char = ""
+        Dim unwantedchar As Char = ""
+        Dim replacingchar As Char = ""
+        Dim loop1 As Integer = 0
+
+        Console.Write("Enter the string: ")
+        mystr = Console.ReadLine
+        Console.Write("Enter the character you want to replace: ")
+        unwantedchar = Console.ReadLine
+        Console.Write("Enter the character you want to replace the older character with: ")
+        replacingchar = Console.ReadLine
+
+        For loop1 = 1 To Len(mystr)
+            chr1 = Mid(mystr, loop1, 1)
+            If chr1 = unwantedchar Then
+                Newstr = Newstr & replacingchar
+            Else : Newstr = Newstr & chr1
+            End If
+        Next
+
+        Console.WriteLine("Enter the string with the characters replaced" & Newstr)
+        Console.ReadKey()
+    End Sub
+
+End Module
